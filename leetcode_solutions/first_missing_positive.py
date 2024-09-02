367294aeb1d3a2ad228b6aadda299ee06f70ea88
Nasifay-Chala <nasifayc11@ethioware.org>
2024-09-02 18:51:06 +0300

    cyclic sort

---
+++ leetcode_solutions/first_missing_positive.py
@@ -0,0 +1,19 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] > 0:
+                correct = nums[i] -1
+                if correct < len(nums) and nums[i] != nums[correct]:
+                    nums[i], nums[correct] = nums[correct], nums[i]
+                else:
+                    i += 1
+            else:
+                i += 1
+        
+        
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
+        return len(nums) + 1
+        
--- leetcode_solutions/set_mismatch.py
+++ leetcode_solutions/set_mismatch.py
@@ -1,9 +1,5 @@
-
-from typing import List
-
-
 class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
         i = 0
         while i < len(nums):
             correct = nums[i] -1
@@ -15,9 +11,8 @@ class Solution:
         result = []
         for i in range(len(nums)):
             if nums[i] != i + 1:
+                result.append(nums[i])
                 result.append(i + 1)
+                break
         return result
-
-    
-
-
+        
