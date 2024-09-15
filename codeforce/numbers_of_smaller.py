bf5913f111cbe103aa9c100873fe437dbb1c2321
Nasifay-Chala <nasifayc11@ethioware.org>
2024-09-15 13:51:33 +0300

    two pointers method

--- codeforce/numbers_of_smaller.py
+++ codeforce/numbers_of_smaller.py
@@ -4,21 +4,22 @@ def merge(arr1, arr2):
     i,j= 0,0
     merged = []
 
-    while i < n or j < m:
+    while j < m:
         if i < n and j < m:
-            if arr1[i] < arr2[j]:
-                merged.append(arr1[i])
+            if arr2[j] > arr1[i]:
                 i += 1
             else:
-                merged.append(arr2[j])
                 j += 1
-        elif i < n:
-            merged.append(arr1[i])
-            i += 1
+                merged.append(i)
         else:
-            merged.append(arr2[j])
             j += 1
+            merged.append(i)
+        
     return merged
+
+
+
+
 
 
 n, m = map(int, input().split())  
