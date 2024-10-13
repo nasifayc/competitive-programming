169597435283e0951e89e858a3345d104428417f
Nasifay-Chala <nasifayc11@gmail.com>
2024-10-13 10:29:10 +0300

    min_matrix_heap

--- leetcode_solutions/k-closest-points-to-origin.py
+++ leetcode_solutions/k-closest-points-to-origin.py
@@ -5,6 +5,8 @@ from typing import List
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         min_heap = [(x**2 + y**2, [x, y]) for x, y in points]
+        
+        # make a priority queue to access the smalles value from the heap
         heapq.heapify(min_heap)
 
         res = []
